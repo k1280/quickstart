f93a9fadabeba426348cc2ac377e0746e06cd792
Katarzyna Wicherska <kawi@gft.com>
2017-02-07 15:07:20 +0100

    add, create, delete methods, update of css for delete button

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,8 +33,11 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
 
   private headers = new Headers({ 'Content-Type': 'application/json' });
@@ -45,6 +48,22 @@ export class HeroService {
       .put(url, JSON.stringify(hero), { headers: this.headers })
       .toPromise()
       .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = '${this.heroesUrl}/${hero.id}';
+    return this.http.delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
       .catch(this.handleError);
   }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
+import { Headers, Http } from '@angular/http';
+
+
 @Component({
   moduleId: module.id,
   selector: 'my-heroes',
@@ -17,6 +20,25 @@ export class HeroesComponent implements OnInit {
   constructor(
     private router: Router,
     private heroService: HeroService) { }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      })
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
