76c5ca393153c58d1f883a94067675ae013ffbd0
Katarzyna Wicherska <kawi@gft.com>
2017-02-07 13:59:08 +0100

    update of imports, fix of spelling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,10 +2,27 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
 
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class HeroService {
+
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
@@ -17,6 +34,6 @@ export class HeroService {
 
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
+      .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
